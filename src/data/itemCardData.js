ee4e83196894a1e5a824a340c63e59260938f5c2
nobuhiro <n.naito1014@gmail.com>
2025-08-10 22:51:58 +0900

    修正

---
+++ src/data/itemCardData.js
@@ -0,0 +1,116 @@
+export const productsFeatured = [
+  {
+    image: 'item-1.jpg',
+    title: '銘茶五香（ごこう）煎茶・抹茶カステラ詰合せ',
+    titleSub: '',
+    price: '2,945',
+    description:
+      '抹茶を使ったスイーツや森半の茶師がこだわり抜いた厳選茶葉のみをブレンドして仕上げた「五香（ごこう）」煎茶です。',
+  },
+  {
+    image: 'item-1.jpg',
+    title: '抹茶スイーツお試しセット',
+    titleSub: '',
+    price: '1,980',
+    description:
+      '森半のスイーツを初めてご利用の方に、人気の3種を詰め合わせたお試しセットです',
+    href: '#',
+  },
+  {
+    image: 'item-1.jpg',
+    title: '京の現代名工・吉田利一作 宇治玉露',
+    titleSub: '［ 100g缶］',
+    price: '10,800',
+    description:
+      '「宇治にこの人あり」名人 吉田利一氏が丁寧に作り上げたまさに「至高の逸品」です。',
+    href: '#',
+  },
+  {
+    image: 'item-1.jpg',
+    title: '五感で味わう急須 CHASTA',
+    titleSub: '（チャスタ）',
+    price: '3,300',
+    description:
+      'これまでの急須では実現できなかった利便性、現代のライフスタイルに寄り添った多用途が特徴の次世代茶器',
+    href: '#',
+  },
+];
+
+export const productsDelicious = [
+  {
+    image: 'item-1.jpg',
+    title: '薄焼き ロールクレープ 抹茶くるる',
+    titleSub: '',
+    price: '1,663',
+    description:
+      '薄焼きクレープを丸めて生地にし、内側に抹茶チョコレートを流し込みました。',
+  },
+  {
+    image: 'item-1.jpg',
+    title: '抹茶フィナンシェ・抹茶ヴァッフェル詰合せ',
+    titleSub: '',
+    price: '1,576',
+    description:
+      'ちょっとした手土産にピッタリの「抹茶ヴァッフェル」「抹茶フィナンシェ」の詰め合わせギフトです。',
+    href: '#',
+  },
+  {
+    image: 'item-1.jpg',
+    title: 'クリームサンド ヴァッフェル（抹茶・ほうじ茶）詰合せ',
+    titleSub: '',
+    price: '1,954',
+    description:
+      'お菓子好きの方へ贈り物をお考えなら森半の「ヴァッフェル詰め合わせ」はいかがですか？',
+    href: '#',
+  },
+  {
+    image: 'item-1.jpg',
+    title: '極上手作り宇治抹茶カステラ',
+    titleSub: '［5切］',
+    price: '1,058',
+    description:
+      '上級の宇治抹茶をたっぷり使って熟練の職人の手で焼き上げた極上カステラ です。',
+    href: '#',
+  },
+];
+
+export const productsRelaxation = [
+  {
+    image: 'item-1.jpg',
+    title: 'CHASTA（チャスタ）・ISSEN ギフトセット',
+    titleSub: '',
+    price: '5,390',
+    description:
+      '日常を整える、とっておきの急須時間を届いてすぐに五感で楽しめるので、ご自宅用はもちろん大切な方への贈り物におすすめです。',
+    href: '#',
+  },
+  {
+    image: 'item-1.jpg',
+    title: '〈宇治〉 名品銘茶 詰め合わせ（高山寺）',
+    titleSub: '',
+    price: '5,400',
+    description:
+      '全国茶審査技術大会で2度の優勝を誇る茶師が、自信を持ってお勧めする玉露と煎茶のギフトセットです。',
+    href: '#',
+  },
+  {
+    image: 'item-1.jpg',
+    title: '〈 五香 （ごこう）〉3種詰め合わせ',
+    titleSub: '［煎茶・雁が音・抹茶入り玄米茶 各 1 袋 ］',
+    price: '2,970',
+    description:
+      '森半の茶師がこだわり抜いた厳選茶葉のみをブレンドして仕上げた「五香（ごこう）」シリーズの普段使いしやすい3種詰め合わせ。',
+    href: '#',
+  },
+  {
+    image: 'item-1.jpg',
+    title:
+      'KFK（カフカ ） 〈ドリップコーヒー〉ロマンスコーヒーセレクションギフトセット',
+    titleSub: '',
+    price: '2,268',
+    description:
+      '焙煎、鮮度、品質にこだわった人の心を魅了するワンランク上のコーヒーのギフトセットです。',
+    href: '#',
+  },
+];
+
