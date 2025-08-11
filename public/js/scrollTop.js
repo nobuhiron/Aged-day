bd869921ec9d3f0d18f73ab32cb59d192866cb4c
nobuhiro <n.naito1014@gmail.com>
2025-08-12 07:33:37 +0900

    修正

--- public/js/scrollTop.js
+++ public/js/scrollTop.js
@@ -1,7 +1,7 @@
 const scrollTopBtn = document.querySelector('.c-scroll-top');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 200) {
+  if (window.scrollY > 800) {
     scrollTopBtn.classList.add('is-active');
   } else {
     scrollTopBtn.classList.remove('is-active');
